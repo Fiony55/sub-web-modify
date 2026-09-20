53f68a897d1aeb3734e505715af41a74c08f1634
Zing <xiaokui5@live.cn>
2026-09-20 22:09:40 +0800

    Update print statement to say 'Goodbye World'

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -348,12 +348,14 @@ export default {
           "dlj.tf": "https://dlj.tf/short",
         },
         customBackend: {
+          "自用专属后端": "https://subapi.zing.ccwu.cc",// 为您的后端取名
           "CM提供-负载均衡后端": "https://subapi.cmliussss.net",
           "CM提供-应急备用后端": "https://subapi.fxxk.dedyn.io",
           "肥羊提供-增强型后端": "https://url.v1.mk",
           "肥羊提供-备用后端": "https://api.v1.mk",
         },
         backendOptions: [
+          { value: "https://subapi.zing.ccwu.cc" }, // 添加您的后端地址
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
